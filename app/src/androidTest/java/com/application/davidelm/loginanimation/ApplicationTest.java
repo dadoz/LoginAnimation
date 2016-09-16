4bd34ba6691988c1d179a7a3ec724b67beff7572
davide-ennova <davide.lamanna@ennova.it>
2016-09-16 17:40:44 +0200

    added espresso testSuite

--- app/src/androidTest/java/com/application/davidelm/loginanimation/ApplicationTest.java
+++ app/src/androidTest/java/com/application/davidelm/loginanimation/ApplicationTest.java
@@ -1,13 +1,27 @@
 package com.application.davidelm.loginanimation;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ApplicationTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void firstSampleTest() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
     }
 }
